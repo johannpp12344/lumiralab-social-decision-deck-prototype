eddbbc82d209e25e25eb586c88bb2b464aae9a33
johannpp12344 <pollupatch@gmail.com>
2026-09-19 16:15:01 +0200

    Prepare usability test mode (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lumira-social-decision-deck-v2";
+const CACHE = "lumira-social-decision-deck-v3";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
